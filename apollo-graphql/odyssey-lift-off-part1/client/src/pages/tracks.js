c7fce4f0763bea24bf004e3803c564c2c67c0d63
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-09-23 15:37:56 -0700

    usequery

--- apollo-graphql/odyssey-lift-off-part1/client/src/pages/tracks.js
+++ apollo-graphql/odyssey-lift-off-part1/client/src/pages/tracks.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Layout } from '../components';
 import {useQuery, gql} from '@apollo/client';
 import TrackCard from '../containers/track-card';
+import { Layout, QueryResult } from '../components';
 
 /** TRACKS query to retrieve all tracks */
 const TRACKS = gql`
@@ -27,15 +27,13 @@ const TRACKS = gql`
  const Tracks = () => {
   const {loading, error, data} = useQuery(TRACKS);
 
-  if (loading) return 'Loading...';
-
-  if (error) return `Error! ${error.message}`;
-
   return (
     <Layout grid>
-      {data?.tracksForHome?.map(track => (
-        <TrackCard key={track.id} track={track} />
-      ))}
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map(track => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
     </Layout>
   );
 };
