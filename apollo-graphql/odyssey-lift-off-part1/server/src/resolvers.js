64ded352a27e58d80c08d50f37e4af7ca673c652
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-09-26 11:39:41 -0700

    fixed context error

--- apollo-graphql/odyssey-lift-off-part1/server/src/resolvers.js
+++ apollo-graphql/odyssey-lift-off-part1/server/src/resolvers.js
@@ -25,7 +25,7 @@ const resolvers = {
     Query: {
         // returns an array of Tracks that will be used
         // to populate the homepage grid of our web client
-        tracksForHome: (_, __, context, { dataSources }) => {
+        tracksForHome: (_, __, { dataSources }) => {
             return dataSources.trackAPI.getTracksForHome();
         },
     },
