4891d22216aeef8d8d6255c10ba240bbe94a94b2
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-09-29 09:52:39 -0700

    create track page and import into index file with route

---
+++ apollo-graphql/odyssey-lift-off-part1/client/src/pages/track.js
@@ -0,0 +1,8 @@
+import {gql, useQuery} from '@apollo/client';
+import {Layout, QueryResult} from '../components';
+
+const Track = ({trackId}) => {
+    return <Layout></Layout>;
+  };
+
+  export default Track;
