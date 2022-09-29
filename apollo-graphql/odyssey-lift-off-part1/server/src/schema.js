010e9854674d140f55a3763446da965f344e685b
Amanda Godsey <amanda.d.godsey@gmail.com>
2022-09-29 11:20:47 -0700

    added track mutation

--- apollo-graphql/odyssey-lift-off-part1/server/src/schema.js
+++ apollo-graphql/odyssey-lift-off-part1/server/src/schema.js
@@ -9,6 +9,22 @@ const typeDefs = gql`
         tracksForHome: [Track!]!
         track(id: ID!): Track
     }
+
+    type Mutation {
+        incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+    }
+
+    type IncrementTrackViewsResponse {
+        "status of the mutation"
+        code: Int!
+        "indicates whether the mutation was successful"
+        success: Boolean!
+        "message for the UI"
+        message: String!
+        "updated track after a successful mutation"
+        track: Track
+    }
+
     "A track is a group of Modules that teaches about a specific topic"
     type Track {
         id: ID!
